1313f8af96d8e13334a2c449523cd3cc3dcea73b
Enrico Liguori <e.rigorighetti@gmail>
2015-03-18 14:16:27 +0000

    Enhancement to CISCO-VTP MIB

--- lib/SNMP/Insight/MIB/Cisco_VTP.pm
+++ lib/SNMP/Insight/MIB/Cisco_VTP.pm
@@ -11,7 +11,7 @@ use SNMP::Insight::Moose::MIB;
 use namespace::autoclean;
 with 'SNMP::Insight::MIB';
 
-mib_oid "1.3.6.1.4.1.9.9.";
+mib_oid "1.3.6.1.4.1.9.9.46";
 
 mib_name "CISCO-VTP-MIB";
 
@@ -21,17 +21,35 @@ has_scalar 'vtpMaxVlanStorage' => ( oid => '1.1.2', );
 
 # This table contains information on the VLANs which currently exist.
 has_table 'vtpVlanTable' => (
-    oid     => '1.3',
+    oid     => '1.3.1',
     index   => 'vtpVlanIndex',
     columns => {
-        vtpVlanIndex   => 1,
-        vtpVlanState   => 2,
-        vtpVlanType    => 3,
-        vtpVlanName    => 4,
-        vtpVlanTypeExt => 17,
-        vtpVlanIfIndex => 18
+        vtpVlanIndex     => 1,
+        vtpVlanState     => [2, 'munge_state' ],
+        vtpVlanType      => 3,
+        vtpVlanName      => 4,
+        vtpVlanMtu       => 5,
+        vtpVlanDot10Said => 6,
+        vtpVlanTranslationalVlan1 => 11,
+        vtpVlanTranslationalVlan2 => 12,
+        vtpVlanTypeExt   => 17,
+        vtpVlanIfIndex   => 18
     },
 );
+
+sub munge_state {
+    my $val = shift;
+    $val or return;
+    my %ENUM = (
+        1     => 'operational',
+        2     => 'suspended',
+        3     => 'mtuTooBigForDevice',
+        4     => 'mtuTooBigForTrunk',
+    );
+    return $ENUM{$val};
+}
+
+
 
 1;
 
